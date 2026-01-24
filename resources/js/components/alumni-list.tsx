c17236a504a9971f0ee989cd1a632cc3c9ced6dc
Samanseso <winesevander@gmail.com>
2026-01-24 14:12:05 +0800

    Alumni table footer added

--- resources/js/components/alumni-list.tsx
+++ resources/js/components/alumni-list.tsx
@@ -32,14 +32,14 @@ const columns = [
 ];
 
 export default function AlumniList() {
-    const { props } = usePage<{ alumni: Pagination<Alumni[]>, modal: ModalType  }>();
+    const { props } = usePage<{ alumni: Pagination<Alumni[]>, modal: ModalType }>();
 
     const [alumni, setAlumni] = useState<Alumni[]>(props.alumni.data);
     const [searchInput, setSearchInput] = useState('');
     const [selectedStudent, setSelectedStudent] = useState<string>('');
     const [isOpenDeleteModal, setIsOpenDeleteModal] = useState<boolean>(false);
 
-    const [tableVersion, setTableVersion] = useState(0); 
+    const [tableVersion, setTableVersion] = useState(0);
 
 
     const [open, setOpen] = useState(false);
@@ -49,7 +49,7 @@ export default function AlumniList() {
         setSelectedStudent(id)
     }
 
-    useEffect(() => {   
+    useEffect(() => {
         setOpen(false);
         setAlumni(props.alumni.data);
     }, [props.alumni]);
@@ -63,14 +63,14 @@ export default function AlumniList() {
     }, [alumni])
 
     return (
-        <div className="m-4 bg-white shadow rounded-lg h-[100%] overflow-hidden">
+        <div className="m-4 relative bg-white shadow rounded-lg h-[100%] overflow-hidden">
             <div className="flex p-5 pb-2 justify-between">
                 <p className="font-bold text-xl text-gray-600">List of Alumni</p>
                 <div className="flex gap-2">
                     <Button variant="outline" className="hidden md:flex"><Download />Export</Button>
                     <Button variant="outline" className="hidden md:flex" onClick={() => setOpen(true)}>
                         <Upload />Import
-                        </Button>
+                    </Button>
 
                     <Link href={step(1)}>
                         <Button><Plus />Add Alumni</Button>
@@ -86,20 +86,22 @@ export default function AlumniList() {
                 </div>
 
                 <div className="flex gap-2">
-
                     <SearchBar />
                 </div>
 
             </div>
 
-            { alumni.length > 0 && <AlumniTable key={tableVersion} alumni={alumni} columns={columns} /> }
+            {alumni.length > 0 && <AlumniTable key={tableVersion} alumni={alumni} columns={columns} />}
 
             <Import open={open} table="alumni" setOpen={setOpen} />
-        
+
             {props.modal && props.modal.status && <Modal key={tableVersion + 1} content={props.modal} />}
 
+            <div className="flex w-full justify-between items-end px-6 absolute bottom-6 right-0 space-x-10">
+                <p className="text-sm">Showing {props.alumni.data.length} out of {props.alumni.total} entries</p>
+                <TablePagination data={props.alumni} />
+            </div>
 
-            {/* <TablePagination data={props.alumni} /> */}
 
         </div>
 
--- resources/js/components/table-pagination.tsx
+++ resources/js/components/table-pagination.tsx
@@ -43,7 +43,7 @@ export function TablePagination ({ data }: TablePaginationProps) {
     const visiblePages = getVisiblePages(data.links, data.current_page, data.last_page, 1);
 
     return (        
-        <div className="flex justify-between absolute bottom-6 right-0 pr-8 space-x-10">
+        <div className="flex justify-between gap-2">
             {/* I don't know why but pr-8 works ------------------------^ */}
             <div>
                 <Link href={data.prev_page_url ?? ""}>
